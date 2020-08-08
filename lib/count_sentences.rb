d07dbe3cf08993cc12b5cec494656e8534e2c4c0
Alan Coomes <acoomes1@cord.edu>
2020-08-08 00:02:59 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
 
   def count_sentences
-    self.split([/.?!/]).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+  self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
   
 end
